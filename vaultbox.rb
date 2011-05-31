9f768d997260026031a4183ea1bb88b7b13a8cb8
Patrick Collison <patrick@collison.ie>
2011-05-31 13:46:03 -0700

    initial

---
+++ vaultbox.rb
@@ -0,0 +1,150 @@
+require 'rubygems'
+require 'fileutils'
+require 'socket'
+require 'grit'
+require 'ruby_gpg'
+
+module RubyGpg
+  def gpg_command
+    "#{config.executable} --homedir #{config.homedir} --quiet --yes"
+  end
+end
+
+# This implementation leaks:
+# - names of passwords
+# - number of passwords stored per service
+# - approximate size of passwords
+# - timestamps
+
+module VaultBox
+  class Config
+    Default = {
+      :dir => '~/Dropbox/vaultbox',
+      :key => '5989B94E'
+    }
+
+    def self.get(k); Default.fetch(k); end
+    def self.dir; File.expand_path(get(:dir)); end
+    def self.hostname; Socket.gethostname; end
+    def self.key; get(:key); end
+  end
+
+  module FS
+    class FSError < StandardError; end
+
+    extend self
+
+    def dir; Config.dir; end
+
+    def ls(dir)
+      Dir.entries(dir).reject{|e| e =~ /^\./}
+    end
+
+    def passwords
+      ls(self.dir)
+    end
+
+    def init
+      FileUtils.mkdir_p(dir)
+      Grit::Repo.init(dir)
+    end
+
+    def gitpath
+      File.join(dir, '.git')
+    end
+
+    def repo
+      @repo ||= Grit::Repo.new(dir)
+    end
+
+    def commit(*f)
+      Dir.chdir(dir) do
+        self.repo.add(*f)
+        t = Time.now.utc.iso8601
+        h = Config.hostname
+        self.repo.commit_all("Automated commit at #{t} on #{h}")
+      end
+    end
+
+    def latest(name)
+      d = File.join(dir, name)
+      if File.directory?(d)
+        if last = ls(d).sort.last
+          File.join(d, last)
+        else
+          raise FSError.new("Password list for #{name} is empty")
+        end
+      else
+        raise FSError.new("No such password: #{name}")
+      end
+    end
+
+    def store(name, str)
+      d = File.join(dir, name)
+      p = File.join(dir, name, Time.now.utc.iso8601)
+      FileUtils.mkdir_p(d)
+      File.open(p, 'w') {|f| f.write(str)}
+      commit(d, p)
+    end
+  end
+
+  module Crypto
+    extend self
+
+    def encrypt(str)
+      RubyGpg.encrypt_string(str, Config.key, :armor => true)
+    end
+
+    def decrypt(str)
+      RubyGpg.decrypt_string(str)
+    end
+  end
+
+  def self.get
+    if k = ARGV.shift
+      fn = FS.latest(k)
+      puts "Reading #{fn}..."
+      puts Crypto.decrypt(File.read(fn))
+    else
+      usage
+    end
+  end
+
+  def self.set
+    if k = ARGV.shift
+      puts "Reading data up to EOF..."
+      FS.store(k, Crypto.encrypt($stdin.read))
+      puts "Stored as #{k}"
+    else
+      usage
+    end
+  end
+
+  def self.ls
+    puts FS.passwords.join("\n")
+  end
+
+  def self.usage
+    $stderr.puts "#{__FILE__} [get|set|ls] [key]"
+  end
+
+  def self.init
+    if File.directory?(FS.dir)
+      $stderr.puts "#{FS.dir} already exists"
+    else
+      FS.init
+    end
+  end
+
+  def self.main
+    case ARGV.shift
+    when 'get' then get
+    when 'set' then set
+    when 'ls' then ls
+    when 'init' then init
+    else usage
+    end
+  end
+end
+
+VaultBox.main if $0 == __FILE__
